7fc34ec83d84dd65c2596441a5edbed838f2acc2
Lucy Chen <lucyc1225xx@gmail.com>
2023-02-11 23:04:22 -0500

    update

--- simonscript.js
+++ simonscript.js
@@ -6,6 +6,10 @@ let roundNumber;
 function initialize() {
     roundNumber = 1;
     countLabel = document.getElementById("countLabel");
+    simonbutton1 = document.getElementById("simonbutton1");
+    simonbutton2 = document.getElementById("simonbutton2");
+    simonbutton3 = document.getElementById("simonbutton3");
+    simonbutton4 = document.getElementById("simonbutton4");
 }
 
 function checkButton(buttonNum) {
@@ -28,9 +32,14 @@ function startGame() {
     addToPattern();
 }
 
-function addToPattern() {
+async function addToPattern() {
     currentPatternArr.push(getRandomInt(4) + 1);
-    displayAllPatterns();
+    await displayAllPatterns();
+    //TO DO: this should only execute after the displayAllPatterns function is done
+    simonbutton1.disabled = false;
+    simonbutton2.disabled = false;
+    simonbutton3.disabled = false;
+    simonbutton4.disabled = false;
 }
 
 function getRandomInt(max) {
@@ -40,6 +49,22 @@ function getRandomInt(max) {
 //loops through currentPatternArr and 'lights up' the corresponding button
 function displayAllPatterns() {
     console.log(currentPatternArr);
+    simonbutton1.disabled = true;
+    simonbutton2.disabled = true;
+    simonbutton3.disabled = true;
+    simonbutton4.disabled = true;
+
+    for (let i = 0; i < currentPatternArr.length; i++) {
+        let currentButton = eval("simonbutton" + currentPatternArr[i]);
+        setTimeout(() => {
+            setTimeout(() => {
+                currentButton.classList.add("glow");
+            }, parseInt((i + 1) + "000") / 2);
+            setTimeout(() => {
+                currentButton.classList.remove("glow");
+            }, parseInt((i + 2) + "000") / 2);
+        }, parseInt((i + 1) + "000") / 2);
+    }
 }
 
 function reset() {
